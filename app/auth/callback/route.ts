62e6a105f2ea6e3e39b22057e87bba6509eb795b
kusmich-ai <nkusmich@gmail.com>
2025-11-13 15:48:11 -0800

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -12,5 +12,6 @@ export async function GET(request: Request) {
     await supabase.auth.exchangeCodeForSession(code)
   }
 
+  // URL to redirect to after sign in process completes
   return NextResponse.redirect(new URL('/screening', request.url))
 }
