67be1fbceb00a5972be27074d87e21dde8e0d86f
kusmich-ai <nkusmich@gmail.com>
2025-11-27 11:18:55 -0800

    Update ResonanceBreathing.tsx

--- components/ResonanceBreathing.tsx
+++ components/ResonanceBreathing.tsx
@@ -826,7 +826,7 @@ export default function ResonanceBreathing() {
                 opacity: 0.4,
               }}
             >
-              Headphones recommended for binaural audio
+              Headphones recommended for full affect
             </p>
           )}
         </>
