87eb1c89808f3493aef1935c511af6a1901d46fc
kusmich-ai <nkusmich@gmail.com>
2025-11-28 12:07:20 -0800

    Create index.ts

---
+++ lib/templates/index.ts
@@ -0,0 +1,10 @@
+// /lib/templates/index.ts
+
+export { templateLibrary } from './templateLibrary';
+export { processTemplate } from './processTemplate';
+export { selectTemplate } from './selectTemplate';
+export { 
+  getPracticesForStage,
+  getUnlockCriteriaForStage,
+  calculateDaysInStage 
+} from './helpers';
