5110073b75dc3090f897f6c058540c25aff534d3
kusmich-ai <nkusmich@gmail.com>
2026-02-28 16:41:33 -0800

    Update ToolsSidebar.tsx

--- components/ToolsSidebar.tsx
+++ components/ToolsSidebar.tsx
@@ -102,7 +102,7 @@ export default function ToolsSidebar({
   const { open: openNightlyDebrief, Modal: NightlyDebriefModal } = useNightlyDebrief();
   const { open: openLoopDeLooping, Modal: LoopDeLoopingModal } = useLoopDeLooping();
 
-  const currentStagePractices = getStagePractices(progress.currentStage);
+  const currentStagePractices = getScheduledPracticesForDate(progress.currentStage);
   const unlockedTools = getUnlockedOnDemandTools(progress.currentStage);
 
   
